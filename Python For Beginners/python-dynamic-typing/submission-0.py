cb569ee2fca2e3b0832df3a291fef6789c068a0e
Sam Avramov <114106803+samavramov@users.noreply.github.com>
2026-05-15 16:53:26 -0700

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 1
+print(type(variable))
+variable = 1.00
+print(type(variable))
+variable = True
+print(type(variable))
+variable = "hi"
+print(type(variable))
+variable = [0,1,2]
+print(type(variable))
