4f41a176bd108172f5f8a756438fb415e34232cf
Sam Avramov <114106803+samavramov@users.noreply.github.com>
2026-05-24 20:53:13 -0700

    Add: python-custom-heap - submission-1

---
+++ Python Coding Interviews/python-custom-heap/submission-1.py
@@ -0,0 +1,16 @@
+import heapq
+from typing import List
+
+
+def get_reverse_sorted(nums: List[int]) -> List[int]:
+    heap = []
+    for num in nums: 
+        heapq.heappush(heap, (-num, num))
+    return [heapq.heappop(heap)[1] for _ in range(len(heap))]
+
+
+
+# do not modify below this line
+print(get_reverse_sorted([1, 2, 3]))
+print(get_reverse_sorted([5, 6, 4, 2, 7, 3, 1]))
+print(get_reverse_sorted([5, 6, -4, 2, 4, 7, -3, -1]))
