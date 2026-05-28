03c25580637ffb9e7f8783a700467ed28e959d42
Sam Avramov <114106803+samavramov@users.noreply.github.com>
2026-05-28 00:57:03 -0700

    Add: daily-temperatures - submission-28

---
+++ Data Structures & Algorithms/daily-temperatures/submission-28.py
@@ -0,0 +1,14 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        stack = []
+        result = [0] * len(temperatures)
+        for i, t in enumerate(temperatures): 
+                    while stack and stack[-1][1] < t: 
+                        popped = stack.pop()
+                        distance = i - popped[0]
+                        result[popped[0]]=distance
+                    stack.append([i, t])
+        return result
+
+
+
