caf09bee921ef7aead4306ca86682d635572ee35
Sam Avramov <114106803+samavramov@users.noreply.github.com>
2026-06-05 22:29:16 -0700

    Add: minimum-size-subarray-sum - submission-14

---
+++ Data Structures & Algorithms/minimum-size-subarray-sum/submission-14.py
@@ -0,0 +1,15 @@
+class Solution:
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        minwindow = 0
+        count = 0
+        l = 0
+        r = 0
+        for r in range(len(nums)):
+            count += nums[r]
+            while count>=target:
+                distance = r-l+1
+                minwindow = distance if minwindow == 0 else min(minwindow, distance)
+                count -= nums[l]
+                l +=1
+        return minwindow
+        
